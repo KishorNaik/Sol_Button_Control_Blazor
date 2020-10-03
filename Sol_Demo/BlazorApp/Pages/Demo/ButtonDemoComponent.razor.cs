9376a1f49e28e361f43595e6f4afdfe2fb53e0ad
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-03 09:00:30 +0530

    Code Commit

---
+++ Sol_Demo/BlazorApp/Pages/Demo/ButtonDemoComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿using Button;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages.Demo
+{
+    public partial class ButtonDemoComponent
+    {
+        #region Private Button reference
+
+        private ButtonComponent ButtonRed { get; set; }
+
+        private ButtonComponent ButtonGreen { get; set; }
+
+        private ButtonComponent ButtonBlue { get; set; }
+
+        #endregion Private Button reference
+
+        #region Private Property
+
+        private String DisplayText { get; set; }
+
+        #endregion Private Property
+
+        #region Public Event Handler
+
+        public void OnBlueButtonClickEventHandler(MouseEventArgs mouseEventArgs)
+        {
+            DisplayText = "Blue Button Click";
+        }
+
+        public void OnGreenButtonClickEventHandler(MouseEventArgs mouseEventArgs)
+        {
+            DisplayText = "Green Button Click";
+        }
+
+        public void OnRedButtonClickEventHandler(MouseEventArgs mouseEventArgs)
+        {
+            DisplayText = "Red Button Click";
+        }
+
+        #endregion Public Event Handler
+    }
+}
---
+++ Sol_Demo/Button/ButtonComponent.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Button
+{
+    public enum ButtonStyle
+    {
+        blue = 0,
+        red = 1,
+        green = 2
+    };
+
+    public partial class ButtonComponent
+    {
+        #region Public Parameter Property
+
+        [Parameter]
+        public ButtonStyle Styles { get; set; }
+
+        [Parameter]
+        public String Text { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClickEventHandler { get; set; }
+
+        #endregion Public Parameter Property
+
+        #region Private Property
+
+        private String StyleName { get; set; }
+
+        #endregion Private Property
+    }
+}
